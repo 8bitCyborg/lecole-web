c71237e7915d6af3104d36e34f3bbd8a3ccb210a
Shalom Junior <shalomjunior007@gmail.com>
2026-04-23 15:47:42 +0100

    pwa foundations

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,19 @@ import { PersistGate } from 'redux-persist/integration/react'
 import { store, persistor } from './store'
 import './index.css'
 import App from './App.tsx'
+import { registerSW } from 'virtual:pwa-register';
+
+// registerSW({ immediate: true })
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm('New version available, click OK to refresh')) {
+      updateSW()
+    }
+  },
+  onOfflineReady() {
+    console.log('App is ready to work offline')
+  },
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,36 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'Lecole',
+        short_name: 'Lecole',
+        description: 'Lecole',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/logo-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/logo-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
   ],
   resolve: {
     alias: {
